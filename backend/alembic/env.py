eff6a5625269f5ce662be2b081d7dc54bc72f657
hchas <hchas@local>
2026-02-26 17:03:11 -0600

    Lint:

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,6 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from app.core.config import get_settings
 from app.db.base import Base
 from app import models  # noqa: F401
 
